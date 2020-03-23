919dae8c6eeb5355a393431f5ad0b381ccd178cd
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-23 16:18:02 +0700

    Create Constants.rb

---
+++ lib/Constants.rb
@@ -0,0 +1,16 @@
+module Namdevel
+  class Constants
+
+    API = 'https://api.ovo.id'
+    AWS_API = 'https://apigw01.aws.ovo.id'
+    OS_NAME = 'iOS'
+    OS_VERSION = '13.3.1'
+    APP_ID = 'P72RVSPSF61F72ELYLZI'
+    APP_VERSION = '3.6.0'
+    USER_AGENT = 'OVO/3.6.0 (ovo.id; build:8139; iOS 13.3.1) Alamofire/4.7.3'
+    ACTION_MARK = 'OVO Cash'
+    DEVICE_ID = '285B1851-50FA-A1B9-2449-8D75618C6414'
+    PUSH_NOTIFICATION = '7c83e48253d1cd45f84ed261a4a44a6c6b3efebc6f4510911fcb72b42dae369c'
+    
+  end
+end
