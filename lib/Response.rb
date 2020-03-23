65ff5c36bad948f49fd51c540f40a61cd607fcd5
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-23 16:18:06 +0700

    Create Response.rb

---
+++ lib/Response.rb
@@ -0,0 +1,55 @@
+module Namdevel
+  class Response
+
+    #getMessage
+    def getMessage(response)
+      json = JSON.parse(response)
+      return json['message']
+    end
+
+     #getRefId
+    def getRefId(response)
+      json = JSON.parse(response)
+      refId = json['refId']
+      if(refId)
+        return json['refId']
+      else
+        return getMessage(response)
+      end
+    end
+
+    #getupdateAccessToken
+    def getupdateAccessToken(response)
+      json = JSON.parse(response)
+      updateAccessToken = json['updateAccessToken']
+      if(updateAccessToken)
+        return json['updateAccessToken']
+      else
+        return getMessage(response)
+      end
+    end
+
+    #getToken
+    def geAuthToken(response)
+      json = JSON.parse(response)
+      token = json['token']
+      if(token)
+        return json['token']
+      else
+        return getMessage(response)
+      end
+    end
+
+    #getTrxId
+    def getTrxId(response)
+      json = JSON.parse(response)
+      token = json['trxId']
+      if(token)
+        return json['trxId']
+      else
+        return getMessage(response)
+      end
+    end
+    
+  end
+end
