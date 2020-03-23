fb2225286b0cb4c8a4fcb2fabd3fc94d82e90549
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-23 16:18:30 +0700

    Create walletInquiry.rb

---
+++ example/walletInquiry.rb
@@ -0,0 +1,7 @@
+require '../lib/Ovo'
+
+# @see getAuthToken.rb to get Authorization Token
+# @return json
+authToken = 'eyJhbGciOiJSUzI1NiJ9.eyJleHBpcn......'
+ovo = Namdevel::Ovo.new(authToken)
+puts ovo.walletInquiry
